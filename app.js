5a48d86e74558a9e5b25518fde548c209d334a2b
kritika21012004 <103102183+kritika21012004@users.noreply.github.com>
2024-05-02 14:49:03 +0530

    server file added

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 console.log("app.js file added");
 console.log(new Date());
+const app= require('express')();
+app.get('/',(req,res)=>{
+    res.json({message:'webserver running'});
+})
+app.listen(7000,()=>{
+    console.log("server running on 7000");
+})
