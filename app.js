e8c399e3a888d91701c13478681d2a9afc9b93d7
kritika21012004 <103102183+kritika21012004@users.noreply.github.com>
2024-05-03 14:44:55 +0530

    message changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log("app.js file added");
 console.log(new Date());
 const app= require('express')();
 app.get('/',(req,res)=>{
-    res.json({message:'webserver running'});
+    res.json({message:'webserver running with ci cd'});
 })
 app.listen(7000,()=>{
     console.log("server running on 7000");
